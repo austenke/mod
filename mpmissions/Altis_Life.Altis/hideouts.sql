8e4bb3245ed6de14f7c606807087eaf752180893
Austen Keene <austenkeene@gmail.com>
2015-12-24 17:40:55 -0500

    Add sql file for hideouts

---
+++ mpmissions/Altis_Life.Altis/hideouts.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `hideouts` (
+  `id` int NOT NULL,
+  `gangName` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+INSERT INTO `hideouts` VALUES ('1','Uncontested')
+INSERT INTO `hideouts` VALUES ('2','Uncontested')
+INSERT INTO `hideouts` VALUES ('3','Uncontested')
