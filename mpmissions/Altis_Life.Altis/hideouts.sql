a202d00e63c7f97983922a36fe592af0c4c969da
austenke <austenkeene@gmail.com>
2015-12-24 17:50:08 -0500

    Sql changes successful

--- mpmissions/Altis_Life.Altis/hideouts.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `hideouts` (
-  `id` int NOT NULL,
-  `gangName` varchar(32) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-
-INSERT INTO `hideouts` VALUES ('1','Uncontested')
-INSERT INTO `hideouts` VALUES ('2','Uncontested')
-INSERT INTO `hideouts` VALUES ('3','Uncontested')
