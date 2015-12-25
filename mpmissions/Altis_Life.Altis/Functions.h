77dcc4a3a6b197a6ccb01fdd7385c6652cf6f05b
valiant3 <jakewoodall3@gmail.com>
2015-12-25 00:05:26 -0500

    Cartels Part 3

--- mpmissions/Altis_Life.Altis/Functions.h
+++ mpmissions/Altis_Life.Altis/Functions.h
@@ -306,6 +306,8 @@ class Life_Client_Core
 		class gangDisband {};
 		class gangDisbanded {};
 		class gangMarkers {};
+		class toggleCaptureUI {};
+		class updateCaptureUI {};
 	};
 	
 	class Shops
