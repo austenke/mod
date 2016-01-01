189e612c49f4fa0b02dfb1a8cd7d1328c932d28e
Austen Keene <austenkeene@gmail.com>
2015-12-31 19:24:16 -0500

    remove give key

--- mpmissions/Altis_Life.Altis/dialog/key_chain2.hpp
+++ mpmissions/Altis_Life.Altis/dialog/key_chain2.hpp
@@ -77,15 +77,15 @@ class Life_key_management2 {
 			h = (1 / 25);
 		};
 		
-		class GiveKey : Life_RscButtonMenu {
-			idc = 9953;
-			text = "$STR_Keys_GiveKey";
-			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", 0.5};
-			onButtonClick = "[] call life_fnc_keyGive";
-			x = 0.32;
-			y = 0.69;
-			w = (6.25 / 40);
-			h = (1 / 25);
-		};
+		//class GiveKey : Life_RscButtonMenu {
+		//	idc = 9953;
+		//	text = "$STR_Keys_GiveKey";
+		//	colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", 0.5};
+		//	onButtonClick = "[] call life_fnc_keyGive";
+		//	x = 0.32;
+		//	y = 0.69;
+		//	w = (6.25 / 40);
+		//	h = (1 / 25);
+		//};
 	};
 };
