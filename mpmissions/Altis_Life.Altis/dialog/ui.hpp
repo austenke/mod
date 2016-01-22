2df769aa8c382a7d9952dd21c928a1df8c0faed2
Austen Keene <austenkeene@gmail.com>
2016-01-22 15:46:56 -0500

    Text tweak

--- mpmissions/Altis_Life.Altis/dialog/ui.hpp
+++ mpmissions/Altis_Life.Altis/dialog/ui.hpp
@@ -80,7 +80,7 @@ class playerHUD {
 		idc = 1003;
 		text = "";
 		x = 0.958906 * safezoneW + safezoneX;
-		y = 0.797 * safezoneH + safezoneY;
+		y = 0.8014 * safezoneH + safezoneY;
 		w = 0.020625 * safezoneW;
 		h = 0.011 * safezoneH;
 	};
